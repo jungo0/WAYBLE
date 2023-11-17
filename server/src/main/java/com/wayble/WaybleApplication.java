28e51f444be72fcceb1b2358e1d50b80fe19cb80
ljg980708 <ljg980708@gmail.com>
2023-11-17 16:22:40 +0900

    feat: default server

---
+++ server/src/main/java/com/wayble/WaybleApplication.java
@@ -0,0 +1,14 @@
+package com.wayble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class WaybleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaybleApplication.class, args);
+    }
+}
